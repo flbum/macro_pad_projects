e87b5447a906731e485732833551a2e42d72afa7
flbum <matt@thebrokebros.com>
2025-04-17 22:42:42 -0400

    on the grind

---
+++ champ_deck/ongoing/champ_deck_4.17.1045/keymaps/vial/keymap.c
@@ -0,0 +1,14 @@
+#include QMK_KEYBOARD_H
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+#endif
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_PLAY,
+                 KC_A,    KC_B,    KC_C
+    )
+};
+
