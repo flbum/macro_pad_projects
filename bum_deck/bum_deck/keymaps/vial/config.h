81cd45fdfc5ba4f67b553d80307e3eff779bea34
flbum <matt@thebrokebros.com>
2025-05-07 16:20:58 -0400

    Added bum_deck

---
+++ bum_deck/bum_deck/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x25, 0x80, 0xD3, 0x37, 0xFB, 0xEB, 0x30, 0x1B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,1}
---
+++ bum_deck/bum_deck/keymaps/vial/keymap.c
@@ -0,0 +1,29 @@
+#include QMK_KEYBOARD_H
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  },
+    [2] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  },
+    [3] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  }
+};
+#endif
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_A,  KC_B,    KC_C,    KC_D,
+        KC_1,  KC_2,    KC_3,    KC_4
+    ),
+    [1] = LAYOUT(
+        _______,  _______,    _______,    _______,
+        _______,  _______,    _______,    _______
+    ),
+    [2] = LAYOUT(
+        _______,  _______,    _______,    _______,
+        _______,  _______,    _______,    _______
+    ),
+    [3] = LAYOUT(
+        _______,  _______,    _______,    _______,
+        _______,  _______,    _______,    _______
+    )
+};
